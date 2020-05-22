57a438d4c01b0c527f07a87c65d4ed26f84b215e
pcborges <delyto@gmail.com>
2020-05-22 18:44:24 -0300

    Structuring folders and files.

---
+++ src/pages/layout/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components' 
+import {colors} from '../../../styles/theme.js'
+
+export const Container = styled.div`
+  background-color: ${colors.primary} ;
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const colors = {
+    primary: '#ff9d43',
+    grey: "#f9f8f8",
+    orange: "#f55d16",
+    dark: "#3e3731",
+    mediumGrey: "#e0e0e0",
+    blue: "#50c1ef",
+};
+
+
