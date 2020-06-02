3718d5d0e5d3b0b7e9d8601d7a27a775f837af17
pcborges <delyto@gmail.com>
2020-06-01 23:54:25 -0300

    using hook usecontext at products

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Cards } from "./styles";
 import Card from "../../components/Card";
 import SubHeader from "../../components/layout/SubHeader";
@@ -6,23 +6,20 @@ import SubHeader from "../../components/layout/SubHeader";
 import ShopContext from "../../context/shop-context";
 
 export default function Products() {
+  const { products, addProductToCart } = useContext(ShopContext);
   return (
-    <ShopContext.Consumer>
-      {(context) => (
-        <>
-          <SubHeader />
-          <Cards>
-            {context.products.map((item, index) => (
-              <Card
-                item={item}
-                badge={item.badge}
-                key={index}
-                addToCart={() => context.addProductToCart(item)}
-              />
-            ))}
-          </Cards>
-        </>
-      )}
-    </ShopContext.Consumer>
+    <>
+      <SubHeader />
+      <Cards>
+        {products.map((item, index) => (
+          <Card
+            item={item}
+            badge={item.badge}
+            key={index}
+            addToCart={() => addProductToCart(item)}
+          />
+        ))}
+      </Cards>
+    </>
   );
 }
